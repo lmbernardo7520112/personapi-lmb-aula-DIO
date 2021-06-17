0493766ca9df5b0076d4a8e47923e245741ca55e
root <root@DESKTOP-1BAE7AU.localdomain>
2021-06-17 18:49:39 -0300

    Class part 8 - Developed delete by id operation.

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -45,13 +45,24 @@ public class PersonService {
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
         //Optional<Person> optionalPerson = personRepository.findById(id);
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
 
         //if (optionalPerson.isEmpty()) {
             //throw  new PersonNotFoundException(id);
         //}
         //return personMapper.toDTO(optionalPerson.get());
         return personMapper.toDTO(person);
+    }
+
+
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
