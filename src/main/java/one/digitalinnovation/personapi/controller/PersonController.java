d5629c45da2c968e10877e927289f88f05347ced
root <root@DESKTOP-1BAE7AU.localdomain>
2021-06-17 15:51:54 -0300

    Class part 8 - Developed list all method.

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -7,14 +7,11 @@ import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 //import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
@@ -35,6 +32,9 @@ public class PersonController {
         return personService.createPerson(personDTO);
     }
 
-
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
 }
 
